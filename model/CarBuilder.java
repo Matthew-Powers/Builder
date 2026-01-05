d99f853fb466d00a0f83308ba460071f84318040
MP <mpowers16@ewu.edu>
2026-01-05 07:48:15 -0800

    Added/implemented CarBuilder

---
+++ model/CarBuilder.java
@@ -0,0 +1,66 @@
+package Builder;
+
+public class CarBuilder implements Builder
+{
+    private int id;
+    private int height;
+    private String brand;
+    private String model;
+    private String color;
+    private String engine;
+    private String numberOfDoors;
+
+    @Override
+    public CarBuilder id(int id)
+    {
+        this.id = id;
+        return this;
+    }
+
+    @Override
+    public CarBuilder brand(String brand)
+    {
+        this.brand = brand;
+        return this;
+    }
+
+    @Override
+    public CarBuilder model(String model)
+    {
+        this.model = model;
+        return this;
+    }
+
+    @Override
+    public CarBuilder color(String color)
+    {
+        this.color = color;
+        return this;
+    }
+
+    @Override
+    public CarBuilder height(int height)
+    {
+        this.height = height;
+        return this;
+    }
+
+    @Override
+    public CarBuilder engine(String engine)
+    {
+        this.engine = engine;
+        return this;
+    }
+
+    @Override
+    public CarBuilder numberOfDoors(int numberOfDoors)
+    {
+        this.numberOfDoors = numberOfDoors;
+        return this;
+    }
+
+    public Car build()
+    {
+        return new Car(id, brand, model, color, height, engine, numberOfDoors);
+    }
+}
