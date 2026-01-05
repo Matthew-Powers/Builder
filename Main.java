1ebf0aa7ff5740742972667b932116ec17496e1b
MP <mpowers16@ewu.edu>
2026-01-05 08:03:33 -0800

    Added/implemented Main

--- Main.java
+++ Main.java
@@ -1,9 +1,22 @@
 package Builder;
 
+import Builder.model.CarBuilder;
+import Builder.model.CarSchemaBuilder;
+
 public class Main
 {
     public static void main(String[] args)
     {
-        
+        Director director = new Director();
+
+        CarBuilder builder = new CarBuilder();
+        director.buildBugatti(builder);
+        builder.model("Chiron");
+        System.out.println(builder.build());
+
+        CarSchemaBuilder schemaBuilder = new CarSchemaBuilder();
+        director.buildLambo(schemaBuilder);
+        schemaBuilder.engine("90").numberOfDoors(3);
+        System.out.println(schemaBuilder.build());
     }
 }
