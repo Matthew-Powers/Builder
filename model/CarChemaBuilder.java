413f08008a43b7871ee622b7ff77d0d61652e416
MP <mpowers16@ewu.edu>
2026-01-05 07:56:26 -0800

    Added/implemented CarSchemaBuilder

---
+++ model/CarChemaBuilder.java
@@ -0,0 +1,66 @@
+package Builder;
+
+public class CarSchemaBuilder implements Builder
+{
+    private int id;
+    private int height;
+    private String brand;
+    private String model;
+    private String color;
+    private String engine;
+    private int numberOfDoors;
+
+    @Override
+    public CarSchemaBuilder id(int id)
+    {
+        this.id = id;
+        return this;
+    }
+
+    @Override
+    public CarSchemaBuilder brand(String brand)
+    {
+        this.brand = brand;
+        return this;
+    }
+
+    @Override
+    public CarSchemaBuilder model(String model)
+    {
+        this.model = model;
+        return this;
+    }
+
+    @Override
+    public CarSchemaBuilder color(String color)
+    {
+        this.color = color;
+        return this;
+    }
+
+    @Override
+    public CarSchemaBuilder height(int height)
+    {
+        this.height = height;
+        return this;
+    }
+
+    @Override
+    public CarSchemaBuilder engine(String engine)
+    {
+        this.engine = engine;
+        return this;
+    }
+
+    @Override
+    public CarSchemaBuilder numberOfDoors(int numberOfDoors)
+    {
+        this.numberOfDoors = numberOfDoors;
+        return this;
+    }
+
+    public CarSchema build()
+    {
+        return new CarSchema(id, brand, model, color, height, engine, numberOfDoors);
+    }
+}
